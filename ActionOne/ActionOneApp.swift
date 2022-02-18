0ce5bf19e1566634232b95d6d0b2964ceb656599
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-02-18 09:37:58 +0000

    Initial Commit

---
+++ ActionOne/ActionOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionOneApp.swift
+//  ActionOne
+//
+//  Created by Srinivas Rowdur on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct ActionOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActionOne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ActionOne
+//
+//  Created by Srinivas Rowdur on 2/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
